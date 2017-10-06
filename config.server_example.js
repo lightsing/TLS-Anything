1544803770f6dd46d5326fd1b514348104db8cfc
lightsing <light.tsing@gmail.com>
2017-10-07 01:59:48 +0800

    refactor config example

--- config.server_example.js
+++ config.server_example.js
@@ -4,21 +4,26 @@ const fs = require('fs');
 
 const options = {
     server: {
-        isServer: true,
-        address: '127.0.0.1',
+        enable: true,
+        listen: {
+            address: '127.0.0.1',
+            port: 1443
+        },
         tlsOptions: {
             key: fs.readFileSync('/path/to/server/cert/key'),
             cert: fs.readFileSync('/path/to/server/cert'),
             requestCert: false,
             // ca: [ fs.readFileSync('/path/to/ca/cert') ]  // client cert verify
         },
-        port: {
+        config: {
             whitelist: true,
+            defaultHost: '172.18.1.3',
+            addressList: ['127.0.0.1', '172.18.1.3'],
             portList: [80, 443, 573, 1080]
         }
     },
     client: {
-        isClient: false
+        enable: false
     }
 }
 
