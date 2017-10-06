ddd1ebe6451b8de2a03d83f7eefd50475f081be6
lightsing <light.tsing@gmail.com>
2017-10-07 04:08:31 +0800

    add server side

--- util.js
+++ util.js
@@ -3,6 +3,7 @@
 const tls = require('tls');
 const net = require('net');
 const fs = require('fs');
+const readline = require('readline');
 
 const getPort = require('get-port');
 
@@ -26,7 +27,18 @@ async function create(options) {
     };
 }
 
-function check(socket, request) {
+const server = config.server;
+var servers = {};
+
+function check(socket, request, addr) {
+    if (request.destroy) {
+        if(servers[addr]) {
+            servers[addr].server.close();
+        }
+        delete servers[addr];
+        socket.emit('destory', {closed: true});
+        return false;
+    }
     request.host = request.host || config.server.config.defaultHost;
     if (!request.port) {
         socket.emit('err', {err: 'Port not set.'});
@@ -46,7 +58,55 @@ function check(socket, request) {
     }
 }
 
+function startServer() {
+    const income = net.createServer(server.tlsOptions, (socket) => {
+        const data = readline.createInterface(socket, socket);
+        data.on('line', (line) => { try {
+            var request = JSON.parse(line);
+            const addr = socket.remoteAddress + ':' + socket.remotePort;
+            if (check(socket, request, addr)) {
+                if (request.create) {
+                    if(!servers[addr]) {
+                        create({
+                            isTLS: request.isTLS || false,
+                            serverOptions: server.tlsOptions,
+                            remote: {
+                                host: request.host,
+                                port: request.port,
+                            }
+                        }).then((res) =>{
+                            res.dhost = request.host;
+                            res.dport = request.port;
+                            servers[addr] = res;
+                            socket.emit('accept', res);
+                    });} else {
+                        socket.emit('accept', servers[addr]);
+                    }
+                }
+            }
+        } catch (ignore) {
+            console.log(ignore);
+            socket.emit('err', {err: 'Server cannot understand.'});
+        }});
+    
+        socket.on('err', (err) => {
+            socket.write(JSON.stringify(err) + '\n');
+            socket.destroy();
+        });
+    
+        socket.on('destroy', (msg) => {
+            socket.write(JSON.stringify(msg)+ '\n');
+            socket.destroy();
+        });
+    
+        socket.on('accept', (res) => {
+            socket.write(JSON.stringify({port:res.port}) + '\n');
+        });
+    });
+    
+    income.listen(config.server.listen);
+}
+
 module.exports = {
-    create: create,
-    check: check
+    startServer: startServer
 }
